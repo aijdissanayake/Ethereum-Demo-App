d06d11703297d230f07e370ec6046cb231f578f1
Achala Dissanayake <aidissanayake@gmail.com>
2018-07-09 17:46:42 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Grid, Button, Form } from 'react-bootstrap';
+import { Table, Grid, Button, Form } from 'react-bootstrap';
 import sampleContract from './sampleContract';
 import web3 from './web3';
 import './App.css';
@@ -11,7 +11,9 @@ class App extends Component {
       currCelsiusTemp: '',
       currKelvinTemp : '',
       newTemp        : null,
-      transactionHash: null
+      transactionHash: null,
+      transactionStatus : null,
+      pending        :0
   };
 
   setNewTempVal = (event) => {
@@ -30,20 +32,35 @@ class App extends Component {
 
     if(defaultAccount && this.state.newTemp){
       console.log('Sending from Metamask account: ' + accounts[0]);
-      await sampleContract.methods.updateTemperature(this.state.newTemp).send({
-        from: accounts[0] 
-      }, (error, transactionHash) => {
+      await sampleContract.methods.updateTemperature(this.state.newTemp).send({from: accounts[0]}, 
+      (error, transactionHash) => {
         //check errors from submitting transacrtions: rejection etc.
         if(error){
           console.log("Transaction was not sent");
           alert("Transaction was not sent!");
         }
         else{
-        console.log(transactionHash);
-        alert("Transaction was successful. TxHash : " + transactionHash);
-        this.setState({transactionHash});
-        }
+          console.log(transactionHash);
+          this.setState({transactionHash});
+          this.setState({blockNumber:"Transaction is Pending"});
+          this.setState({gasUsed:"Transcation is Pending"});
+          this.setState({transactionStatus: "pending"})
+        }        
       });
+        // get Transaction Receipt
+        web3.eth.getTransactionReceipt(this.state.transactionHash, (err, txReceipt)=>{
+          console.log(err,txReceipt);
+          this.setState({transactionStatus: "completed"});
+          this.setState({txReceipt});
+          this.setState({blockNumber: this.state.txReceipt.blockNumber});
+          this.setState({gasUsed: this.state.txReceipt.gasUsed}); 
+        }); //await for getTransactionReceipt
+
+        // while (this.state.transactionStatus === "pending") {
+        //   console.log("inside2");
+        //   this.state.pending === 3 ? this.state.pending = 0 : this.state.pending++ ;
+        //   this.setState({blockNumber: this.state.pending});
+        // }
     }
     else{
       if(!this.state.newTemp){
@@ -97,7 +114,7 @@ class App extends Component {
       <hr/>
       <ul align='left'>
         <li>This is a basic example to get started with Ethereum DApp Development. </li>
-        <li>This doesn't show an example use case of blockchain.</li>
+        <li>This app doesn't really show an example use case of blockchain.</li>
         <li>Refresh Temperature buttons will just query the contract storage. No transacrtion will be initiated. </li>
         <li>Update Temperature will write to the contract storage. A Transaction will be intiated and it need to be mined to complete the updating process.</li>
       </ul>
@@ -143,7 +160,35 @@ class App extends Component {
              Update Temperature(Celsius)
              </Button>
           </Form>
-          {this.state.transactionHash?<div>Tx Hash : {this.state.transactionHash} <a href={txViewURL} target="_blank"> [View Transaction]</a></div>:""}
+          <br/>
+          {/* {this.state.transactionHash?<div>Tx Hash : {this.state.transactionHash} <a href={txViewURL} target="_blank"> [View Transaction]</a></div>:""} */}
+          {this.state.transactionHash? 
+          <Table striped bordered condensed hover>
+                <thead>
+                  <tr>
+                    <th>Transaction Detail</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+               
+                <tbody>
+                  <tr>
+                    <td>Transaction Hash </td>
+                    <td>{this.state.transactionHash} {this.state.transactionHash?<a href={txViewURL} target="_blank">[View Transaction]</a>:""}</td>
+                  </tr>
+
+                  <tr>
+                    <td>Block Number </td>
+                    <td>{this.state.blockNumber}</td>
+                  </tr>
+
+                  <tr>
+                    <td>Gas Used</td>
+                    <td>{this.state.gasUsed}</td>
+                  </tr>                
+                </tbody>
+            </Table>
+          : '' }
       </Grid>
       </div>
     );
